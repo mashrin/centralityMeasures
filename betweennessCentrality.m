d7c7aa69adbeb96db87a2c5fcb3e44be5cd9d6ba
Mashrin Srivastava <mashrin.msrivastava2014@vit.ac.in>
2017-04-08 01:27:12 +0530

    Update and rename betweenness.m to betweennessCentrality.m

--- betweennessCentrality.m
+++ betweennessCentrality.m
@@ -1,89 +1,47 @@
-function betw = node_betweenness_slow(adj)
-
+function betweenfn = node_betweenfneenness_slow(adj)
 n = numnodes(adj);
-
 spaths=inf(n,n);
-
-
 for k=1:n-1
-
   adjk=adj^k;
-
   for i=1:n
-
     for j=1:n
-
       if adjk(i,j)>0
-
         spaths(i,j)=min([spaths(i,j) adjk(i,j)]);
-
       end
-
     end
-
   end
-
 end
-
-
-
-betw = zeros(1,n);  
-
+betweenfn = zeros(1,n);  
 for i=1:n
-
     [J_st,route_st,J,route]=shortest_pathDP(adj,i,i,n);
-
     for j=1:n
-
         if i==j
-
             continue
-
         end
-
-
         [J_ji,step_ind] = min(J(:,j));
-
         route_ji = [j, route(step_ind,j).path];
-
-        betw(route_ji(2:length(route_ji)-1)) = betw(route_ji(2:length(route_ji)-1)) + 1/spaths(j,i);
-
+        betweenfn(route_ji(2:length(route_ji)-1)) = betweenfn(route_ji(2:length(route_ji)-1)) + 1/spaths(j,i);
     end
-
 end
-
-
-
-betw=betw/nchoosek(n,2);
-
-
-
+betweenfn=betweenfn/nchoosek(n,2);
 function n = numnodes(L)
-
 n = length(L);
-
 
 function [J_st,route_st,J,route]=shortest_pathDP(L,s,t,steps)
 
 n = size(L,2);
-
-L(find(L==0))=Inf;  % make all zero distances equal to infinity
-
+L(find(L==0))=Inf;  % zero distances made equal to infinity
 for i=1:n
   J(steps,i) = L(i,t); 
   route(steps,i).path = [t];
 end
-
 for p=1:steps-1
-  k=steps-p; % recurse backwards
-  
+  k=steps-p;
   for i=1:n
     [J(k,i),ind_j] =  min(L(i,:)+J(k+1,:));
     route(k,i).path = [ind_j, route(k+1,ind_j).path];
   end
-  
 end
-
 [J_st,step_ind] = min(J(:,s));
 route_st = [s, route(step_ind,s).path];
 J=J(sort(1:n,'descend'),:);
